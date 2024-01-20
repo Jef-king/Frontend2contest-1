e39f4a72d6c1c1f42f01e618ced9db7065be9e49
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-01-20 20:00:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,25 @@
-/** @format */
-
 let arr = [
-  { id: 1, name: "john", age: "18", profession: "developer" },
-  { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" }
+  { id: 1, name: 'john', age: 18, profession: 'developer' },
+  { id: 2, name: 'jack', age: 20, profession: 'developer' },
+  { id: 3, name: 'karen', age: 19, profession: 'admin' },
 ];
 
-function PrintDeveloperbyMap() {
-  let developer=arr.filter(arr=>arr.profession =='developer');
-  let name=developer.map(developer => developer.name);
-  console.log(name);
-}
 
-function PrintDeveloperbyForEach() {
-  let name=[];
-  arr.forEach(arr=>{
-    if(arr.profession==='developer'){
-      name.push(arr.name);
-    }
-  });
-  console.log(name);
+function PrintDeveloper() {
+let d=arr.filter(arr=>arr.profession==='developer')
+console.log(d);
 }
 
 function addData() {
-  let add={id:4, name:'susan', age:20, profession:'intern'}
-  arr.push(add);
-  console.log(arr);
+let a={id:4,name:'susan',age:20,profession:'intern'};
+arr.push(a);
+console.log(arr);
 }
 
 function removeAdmin() {
-let removeadmin = arr.filter((employee)=>{
-  arr.profession !='admin';
-})
-  console.log(removeadmin);
+  //Write your code here, just console.log
 }
 
 function concatenateArray() {
-  let addarr=[
-    {id:4,name:"jefking",age:"15",profession:"rider"},
-  {id:5,name:"jef",age:"80",profession:"joker"},
-  {id:6,name:"king",age:"21",profession:"coder"}
-  ];
-  let addarrresult= arr.concat(addarr);
-  console.log(addarrresult);
+  //Write your code here, just console.log
 }
