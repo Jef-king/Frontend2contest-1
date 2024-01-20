142c914dfd7b9970ca0b336659a84efb7471509b
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-01-20 21:45:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,15 +17,15 @@ console.log(arr);
 }
 
 function removeAdmin() {
-  arr=arr.filter(removeAdmin=>removeAdmin.profession!="admin");
-  console.log(arr);
+  let removeAdmin=arr.filter(removeAdmin=>removeAdmin.profession!="admin");
+  console.log(removeAdmin);
 }
 
 function concatenateArray() {
  let concatenateArray=[
-  { id: 1, name: 'john', age: 18, profession: 'developer' },
-  { id: 2, name: 'jack', age: 20, profession: 'developer' },
-  { id: 3, name: 'karen', age: 19, profession: 'admin' }
+  { id: 5, name: 'john', age: 19, profession: 'admin' },
+  { id: 6, name: 'jack', age: 23, profession: 'intern' },
+  { id: 7, name: 'karen', age: 29, profession: 'admin' }
  ];
  arr=arr.concat(concatenateArray);
  console.log(arr);
